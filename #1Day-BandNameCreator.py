a87078b38426b2203a083468bce0d64e29029a59
trela <trela.art@gmail.com>
2022-10-15 20:56:38 +0200

    Initial commit

---
+++ #1Day-BandNameCreator.py
@@ -0,0 +1,16 @@
+
+# Name of mini project
+print('Welcome to the Band Name Generator')
+
+# Please input any City name
+print("What's name of the city grew up in")
+cityName = input()
+
+# please input your animals
+print("What's pet lives with you?")
+petName = input()
+
+# Band name line creator below
+print('Your Band name could be :')
+bandName = cityName+"'s " + petName + " " + "band"
+print(bandName)
