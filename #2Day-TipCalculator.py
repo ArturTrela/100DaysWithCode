d7acdcb8f090e2f4843858e238088d9a7abd8ba5
trela <trela.art@gmail.com>
2022-10-17 09:57:40 +0200

    fixed percent value

--- #2Day-TipCalculator.py
+++ #2Day-TipCalculator.py
@@ -5,13 +5,15 @@
 print('Welcome to the tip calculator.')
 bill = float(input('What was the total bill ? $'))
 people = int(input('How many people to split the bill?'))
-percent = int(input('What percent tip would you like to give? 10,12, or 15 '))
+percent_tip = int(input('What percent tip would you like to give? 10,12, or 15 '))
 
 # Math part of code
 
-tip = bill % percent
-to_pay =round((bill + tip) / people, 2)
-
+tip_as_percent = bill * percent_tip/100
+tip_as_percent_head = tip_as_percent / people
+to_pay = round((bill + tip_as_percent) / people, 2)
+total_bill = bill + tip_as_percent
 # Return value for user code
 
-print(F"Each preson should pay : ${to_pay} , included tip {tip}")
+print(F"Each preson should pay : ${to_pay} , included tip {tip_as_percent_head}")
+print(f'Total bill is {total_bill}, and included tip {tip_as_percent}')
