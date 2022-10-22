c6afaf3078ce4aee87507c2afd441c98879a83ad
trela <trela.art@gmail.com>
2022-10-22 15:58:35 +0200

    intro to importing module -RANDOM

---
+++ #Day4 - Random.py
@@ -0,0 +1,15 @@
+import random
+
+
+random_int = random.randint(1, 10)
+print(random_int)
+# 0.0000000 -  0.9999999
+random_float = random.random()
+print(random_float)
+# 0.000000 - 4.9999999
+more_random_float = random_float*5
+print(more_random_float)
+
+
+
+
