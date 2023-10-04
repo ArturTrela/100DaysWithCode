4577b37ae730a6ebda73d3a185a3cfc49c89eafe
trela <trela.art@gmail.com>
2023-10-04 17:08:22 +0200

    update file

--- #12Day -The Number Guessing Game.py
+++ #12Day -The Number Guessing Game.py
@@ -1,6 +1,6 @@
 from ASCIIart import logoNumberGuess
 import random
-#TODO correct display ASCII art LOGO or change type logo
+
 print(logoNumberGuess)
 print(f"Welcome to the Number Guessing Game \n I'm thinking about number in range from 1 to 100 \n"
       f"Please choice your difficulty : 'easy' or 'hard' :")
@@ -18,28 +18,25 @@ else:
 
 guessNumber = random.randint(1, 100)
 inputValue = 0
-condition = 1
-while attempts_qty != 0:
-      #TODO correct decrese value for attempts_qty
-    def numberguess(inputValue, guessNumber, attempts_qty, condition):
-          #TODO - remove additional print statments for clean code
-          if attempts_qty > 0:
-              inputValue = int(input(f'Make a guess :'))
-              print(inputValue)
-              if inputValue < guessNumber:
-                  attempts_qty -= 1
-                  print(guessNumber)
-                  print(f'To low. Remaining ' + str(attempts_qty) + 'attempts')
-              elif inputValue > guessNumber:
-                  attempts_qty -= 1
-                  print(guessNumber)
-                  print(f'To high. Remaining ' + str(attempts_qty) + 'attempts')
-              else:
-                  print(f'Correct! This number is : ' + str(inputValue))
-                  attempts_qty == 0
-          else:
-                #TODO - modified condition for while loop when guess number is correct - maybe use return from function or other\n
-                # check documentation about return result from function
-                condition = False
+while True:
 
-    numberguess(inputValue, guessNumber, attempts_qty,condition)
+    def numberguess(inputValue, guessNumber, attempts_qty,):
+        t = 0
+        if attempts_qty  > 0 or t == 1:
+            inputValue = int(input(f'Make a guess :'))
+            print(inputValue)
+            print(guessNumber)
+            if inputValue < guessNumber:
+                print(f'To low. Remaining ' + str(attempts_qty) + ' attempts')
+            elif inputValue > guessNumber:
+                print(f'To high. Remaining ' + str(attempts_qty) + ' attempts')
+            else:
+                inputValue == guessNumber
+                print(f'Correct! This number is : ' + str(inputValue))
+                attempts_qty == 0
+
+    attempts_qty = attempts_qty - 1
+    numberguess(inputValue, guessNumber, attempts_qty,)
+    print(f'Attempts : ' + str(attempts_qty))
+
+
