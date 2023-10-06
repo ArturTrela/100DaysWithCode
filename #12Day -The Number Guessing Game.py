c8b887ae668d40826541e3f68a10febad6ef6c70
trela <trela.art@gmail.com>
2023-10-06 19:31:47 +0200

    Correct version of guess number game with using "return " in function to abort while loop

--- #12Day -The Number Guessing Game.py
+++ #12Day -The Number Guessing Game.py
@@ -18,25 +18,31 @@ else:
 
 guessNumber = random.randint(1, 100)
 inputValue = 0
-while True:
+def_answer = 0
 
-    def numberguess(inputValue, guessNumber, attempts_qty,):
-        t = 0
-        if attempts_qty  > 0 or t == 1:
-            inputValue = int(input(f'Make a guess :'))
-            print(inputValue)
-            print(guessNumber)
-            if inputValue < guessNumber:
-                print(f'To low. Remaining ' + str(attempts_qty) + ' attempts')
-            elif inputValue > guessNumber:
-                print(f'To high. Remaining ' + str(attempts_qty) + ' attempts')
-            else:
-                inputValue == guessNumber
-                print(f'Correct! This number is : ' + str(inputValue))
-                attempts_qty == 0
 
-    attempts_qty = attempts_qty - 1
-    numberguess(inputValue, guessNumber, attempts_qty,)
-    print(f'Attempts : ' + str(attempts_qty))
+def numberguess(guessNumber, attempts_qty, ):
+    """ Head function of this game where input user's number is compare with
+    hidden number choice from range 1 to 100 """
+    while attempts_qty != 0:
+        inputValue = int(input(f'Make a guess :'))
+        # print(inputValue)
+        # print(guessNumber)
+        if inputValue < guessNumber:
+            print(f'To low.')
+        elif inputValue > guessNumber:
+            print(f'To high.')
+        else:
+            inputValue == guessNumber
+            print(f'Correct! This number is : ' + str(inputValue))
+            attempts_qty == 0
+            return
+        attempts_qty = attempts_qty - 1
+        print(f'Remaining attempts : {attempts_qty}')
+
+        if attempts_qty == 0:
+            print("You lose. Try again ")
+
+numberguess(guessNumber, attempts_qty,)
 
 
