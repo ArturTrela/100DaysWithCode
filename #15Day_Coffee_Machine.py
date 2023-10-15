0d370fd7f8f7673aca0aa1daf7c96ce5d95cbaf3
trela <trela.art@gmail.com>
2023-10-15 23:08:37 +0200

    Add function for coins counting and spending . Add function for check ingredients And add function for report

--- #15Day_Coffee_Machine.py
+++ #15Day_Coffee_Machine.py
@@ -1,54 +1,7 @@
 """Project Coffee Machine from Udemy course #100DaysWithPython"""
-# Coffee Machine Program Requirements
 
-# b. The prompt should show every time action has completed, e.g. once the drink is
-# dispensed. The prompt should show again to serve the next customer.
-
-# 4. Check resources sufficient?
-# a. When the user chooses a drink, the program should check if there are enough
-# resources to make that drink.
-# b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should
-# not continue to make the drink but print: “Sorry there is not enough water.”
-# c. The same should happen if another resource is depleted, e.g. milk or coffee.
-#
-# 5. Process coins.
-# a. If there are sufficient resources to make the drink selected, then the program should
-# prompt the user to insert coins.
-# b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
-# c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
-# pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
-#
-# 6. Check transaction successful?
-# a. Check that the user has inserted enough money to purchase the drink they selected.
-# E.g Latte cost $2.50, but they only inserted $0.52 then after counting the coins the
-# program should say “Sorry that's not enough money. Money refunded.”.
-# b. But if the user has inserted enough money, then the cost of the drink gets added to the
-# machine as the profit and this will be reflected the next time “report” is triggered. E.g.
-# Water: 100ml
-# Milk: 50ml
-# Coffee: 76g
-# Money: $2.5
-# c. If the user has inserted too much money, the machine should offer change.
-# E.g. “Here is $2.45 dollars in change.” The change should be rounded to 2 decimal
-# places.
-#
-# 7. Make Coffee.
-# a. If the transaction is successful and there are enough resources to make the drink the
-# user selected, then the ingredients to make the drink should be deducted from the
-# coffee machine resources.
-# E.g. report before purchasing latte:
-# Water: 300ml
-# Milk: 200ml
-# Coffee: 100g
-# Money: $0
-# Report after purchasing latte:
-# Water: 100ml
-# Milk: 50ml
-# Coffee: 76g
-# Money: $2.5
-# b. Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If
-# latte was their choice of drink.
-from turtle import clear
+# """Dodać aktualizację zasobów w maszynie po wykonaniu kawy"""
+# """Poprawić błąd z próbą przypisania nastepnych składników gdy użyte jest off lub report"""
 
 MENU = {
     "espresso": {
@@ -82,49 +35,119 @@ resources = {
     "money": 0,
 }
 powerOn = 1
-"""Prompt user by asking “What would you like? (espresso/latte/cappuccino):
-   The prompt should show every time action has completed, e.g. once the drink is dispensed.
-   The prompt should show again to serve the next customer."""
-
-
 
 
 def report():
-
     water = resources["water"]
     milk = resources["milk"]
     coffee = resources["coffee"]
     money = resources["money"]
     print(f'In machine remaining ... \nWater: {water} ml\nMilk: {milk} ml\nCoffee: {coffee} g\nMoney: $ {money}')
 
+
+def spend_coins(user_coins, drink_cost):
+    to_spend = user_coins - drink_cost
+    print(f'Your rest of coins is {to_spend} PLN ')
+
+    check_ingredients(resources)
+
+
+def check_ingredients(resources):
+    """Water checking..."""
+    next_water_amount = next_drink_ingredients['water']
+    print(f' Your drink contains  {next_water_amount} ml of water ')
+
+    next_coffee_amount = next_drink_ingredients['coffee']
+    print(f' Your drink contains  {next_coffee_amount} g of coffee ')
+
+    if operation_type(request) != 'espresso':
+        next_milk_amount = next_drink_ingredients['milk']
+        print(f' Your drink contains  {next_milk_amount} ml of milk ')
+
+        if (resources['water'] > next_water_amount and resources['milk'] > next_milk_amount and resources['coffee'] >
+                next_coffee_amount):
+            print(f'Your coffee is preparing ! Please Wait')
+
+    elif resources['water'] > next_water_amount and resources['coffee'] > next_coffee_amount:
+        next_milk_amount = 0
+        print(f'Your coffee is preparing ! Please Wait')
+
+    else:
+        print(f' Not enough ingredients to prepare your coffee ... ')
+        print("Return coins... ")
+
+    return next_water_amount, next_milk_amount, next_coffee_amount
+
+
+def coin_process(drink_cost):
+    correct_cost = 0
+    while not correct_cost:
+
+        print('Please prepare coins to insert into machine ... ')
+
+        dziesiec_groszy = int(input("How many 0.1PLN you have?  ")) * 0.1
+        dwadziescia_groszy = int(input("How many 0.2PLN you have?   ")) * 0.2
+        piecdziesiat_groszy = int(input("How many 0.5PLN you have?  ")) * 0.5
+        zlotowka = int(input("How many 1 PLN you have?  ")) * 1.0
+        dwa_zlote = int(input("How many 2 PLN you have? ")) * 2.0
+        piec_zlotych = int(input("How many 5 PLN you have?  ")) * 5.0
+
+        total_coins = dziesiec_groszy + dwadziescia_groszy + piecdziesiat_groszy + zlotowka + dwa_zlote + piec_zlotych
+        print(f' You insert into machine : {total_coins} PLN')
+        print(f' Your drink cost is : {drink_cost} PLN')
+
+        if drink_cost == total_coins:
+            correct_cost = 0
+            check_ingredients(resources)
+            return "ok"
+        if drink_cost <= total_coins:
+            correct_cost = 0
+            spend_coins(total_coins, drink_cost)
+            return "high"
+        if drink_cost >= total_coins:
+            print(f'Coins value to low please insert coins again ...')
+        else:
+            return "Counting cost still process .. "
+
+
 while powerOn:
     request = input("What would you like? (espresso -Type 'e'/latte - Type 'l' /cappuccino - Type - 'c')\n ").lower()
+
+
     def operation_type(user_choice):
         """Take a coffee type from user."""
 
         if user_choice == 'e':
-            print('Espresso')
+            # print('Espresso ...')
             return 'espresso'
         elif user_choice == 'l':
-            print('Latte')
+            # print('Latte ...')
             return 'latte'
         elif user_choice == 'c':
-            print('Cappuccino')
+            # print('Cappuccino...')
             return 'cappuccino'
         elif user_choice == 'r':
             report()
-            return 'report'
+
         elif user_choice == 'off':
-            return 'off'
-        elif user_choice == "m":
-            print(f'Mleko dla Zuzi')
-        elif user_choice =="z":
-            print('Zelki dla Zuzanki')
+            print('Shutdown ... preparing and report printing')
+            report()
+        # elif user_choice == 't':
+        #     check_ingredients(resources)
+        #     return 'test'
         else:
             print('Something went wrong ! Please type correct operation  type ')
 
-    clear()
 
-    # operation_type(request)
-    # Test returned value from function coffee_type
-    operation_type(request)
+    # (operation_type(request))
+    to_make = operation_type(request)
+    if to_make == 'report' and to_make == 'off':
+        print("Service statement")
+    else:
+        next_drink_ingredients = MENU[to_make]['ingredients']
+        next_drink_cost = MENU[to_make]['cost']
+        print(next_drink_ingredients)
+        print(next_drink_cost)
+
+    if operation_type(request) != "":
+        coin_process(next_drink_cost)
