77e81c6df2fc810a39c068651e80e768c13d2a76
trela <trela.art@gmail.com>
2023-10-16 16:47:11 +0200

    learn using method and give an argument

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-"""Intro into OOP """
-import turtle
+"""Intro into OOP learn to using build method and give an argument for object creating from Class"""
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.shapesize(2)
+timmy.color("pink")
+timmy.pensize(2)
+timmy.pencolor("green")
+timmy.forward(100)
+timmy.tiltangle(-90)
+
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
