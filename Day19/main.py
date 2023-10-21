91077131997ff65c63aca0cbbf8cde79fe2c3216
Artur <trela.art@gmail.com>
2023-10-22 01:38:17 +0200

    add movement after listen press button

--- Day19/main.py
+++ Day19/main.py
@@ -1,30 +1,29 @@
-from turtle import Turtle,Screen
+from turtle import Turtle, Screen
 
 johny = Turtle()
 screen = Screen()
 
 
 def move_fwd():
-    johny.setheading(0)
     johny.forward(10)
+
 
 def move_bwd():
-    johny.setheading(-180)
-    johny.backward(-10)
+    johny.backward(10)
+
 
 def move_right():
-    johny.setheading(90)
-    johny.forward(10)
+    johny.right(5)
+
 
 def move_left():
-    johny.setheading(-90)
-    johny.forward(10)
-
+    johny.left(5)
 
 
 screen.listen()
-screen.onkey(move_fwd,"w")
-screen.onkey(move_bwd,"s")
-screen.onkey(move_left , "a")
-screen.onkey(move_right,"d")
+screen.onkey(move_fwd, "w")
+screen.onkey(move_bwd, "s")
+screen.onkey(move_left, "a")
+screen.onkey(move_right, "d")
+screen.onkey(screen.clear, "c")
 screen.exitonclick()
