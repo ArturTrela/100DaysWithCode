2667bbb4e98ee32191ee3579cfd7447530d35e28
Artur <trela.art@gmail.com>
2023-10-22 05:27:00 +0200

    INIT: Create a new mini project Snake Game with requirement in main.py

---
+++ snake_game/main.py
@@ -0,0 +1,14 @@
+"""
+DAY 1 requirement :
+1. create a snake body
+2. Make a snake
+3. Control the snake
+"""
+
+"""  
+DAY 2 requirement :
+1. Detect collision with food
+2. Create a scoreboard
+3. Detect collision with wall
+4. Detect collision with tail
+"""
