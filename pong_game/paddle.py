1ebeb178b6c69be043945a3c505818a865f96ba0
trela <trela.art@gmail.com>
2023-10-26 05:46:38 +0200

    UPDATE: fix rotation paddle after call paddle_up / paddle_down method. Add class Screen to paddle module

--- pong_game/paddle.py
+++ pong_game/paddle.py
@@ -1,22 +1,31 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 UPPER_EDGE = 50
 LOWER_EDGE = -50
-
+PLAYGROUND_LIMIT_UP = 250
+PLAYGROUND_LIMIT_DOWN = -250
+play_ground = Screen()
 class Paddle(Turtle):
 
     def __init__(self):
         super().__init__()
         self.shape("square")
         self.penup()
-        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.shapesize(stretch_len=5, stretch_wid=1)
         self.color("white")
+        self.setheading(90)
         self.showturtle()
         self.goto(350, 0)
 
     def paddle_up(self):
-        pass
+        if self.ycor() < 240 :
+            self.setheading(90)
+            self.forward(20)
+            play_ground.update()
 
     def paddle_down(self):
-        pass
+        if self.ycor() > - 240 :
+            self.setheading(270)
+            self.forward(20)
+            play_ground.update()
 
 
