e8c7469af516997219f9aefe3a0604626e3553e5
trela <trela.art@gmail.com>
2023-10-28 23:12:19 +0200

    UPDATE: add lines and counter on screen

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,8 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
-from pong_game import paddle
-
-screen =Screen()
+from playground import Liner, Counter
+screen = Screen()
 screen.bgcolor("Black")
 screen.setup(800, 600)
 screen.tracer(0)
@@ -12,12 +11,25 @@ right_paddle = Paddle()
 left_paddle = Paddle()
 left_paddle.color("Blue")
 left_paddle.goto(-350,0)
+new_line = Liner()
 multiPlayer = False
+new_line.make_line()
+new_line.make_playfield()
+player_1_cnt = Counter()
+player_2_cnt = Counter()
+player_1_cnt.goto(50,260)
+player_2_cnt.goto(-50,260)
+player_2_cnt.clear()
+player_1_cnt.clear()
+
 screen.onkey(right_paddle.paddle_up, "Up")
 screen.onkey(right_paddle.paddle_down, "Down")
+player_1_cnt.score_update()
+player_2_cnt.score_update()
 if multiPlayer:
     screen.onkey(left_paddle.paddle_up, "w")
     screen.onkey(left_paddle.paddle_down, "s")
+
 
 screen.update()
 screen.exitonclick()
