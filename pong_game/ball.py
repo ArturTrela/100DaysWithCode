ffbc052d9f9e631fa91406d7f37e5ad81e0f94d1
trela <trela.art@gmail.com>
2023-10-29 21:01:18 +0100

    UPDATE :add for loop to animation ball posiotion and import time lib for additional delay

--- pong_game/ball.py
+++ pong_game/ball.py
@@ -1,6 +1,7 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 import random
-
+import time
+play = Screen()
 
 class Ball(Turtle):
 
@@ -13,20 +14,35 @@ class Ball(Turtle):
         self.goto(0, 0)
 
     def ball_movement(self):
-        y_cord = self.ycor()
+        y_cord = int(self.ycor())
         x_cord = self.xcor()
-        random_angle = random.randint(0, 90)
+        random_angle = random.randint(0, 180)
         self.right(random_angle)
-        if y_cord < 270 or y_cord > - 270:
-            self.forward(50)
-        print(random_angle)
+        condition_y = int(y_cord) > -230 or int(y_cord) < 230
+        condition_x = int(x_cord) > -300 or int(x_cord) < 300
+
+        # for s in range(40):
+        #     self.forward(10)
+        #     print(random_angle)
+        #     play.update()
+        #     time.sleep(0.2)
+        if condition_x or condition_y:
+            for s in range(40):
+                self.forward(10)
+                y_cord = self.ycor()
+                x_cord = self.xcor()
+                print(y_cord)
+                play.update()
+                time.sleep(0.1)
+            print('wall detection')
 
 
 
 
     def wall_detect(self):
         """ Detect a wall collision and create a 90 angle rebound """
-        pass
+        if int(self.ycor()) == -270 or self.ycor() == 270:
+            print("wall detect method")
 
     def paddle_detect(self):
         """Detect ball position in range covered front edge of paddle if not in range call score increse"""
