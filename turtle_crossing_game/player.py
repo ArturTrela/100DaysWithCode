535b6c8299b48361eac0ad75f76b6015ea03d160
trela <trela.art@gmail.com>
2023-10-31 22:33:10 +0100

    UPDATE  : add a player on screen and add his move to upward

--- turtle_crossing_game/player.py
+++ turtle_crossing_game/player.py
@@ -1,7 +1,19 @@
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
+starting_position =(0, -280)
+from turtle import Turtle
 
+class Player(Turtle):
 
-class Player:
-    pass
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.setheading(90)
+        self.goto(starting_position)
+        self.showturtle()
+
+    def move(self):
+        self.forward(10)
