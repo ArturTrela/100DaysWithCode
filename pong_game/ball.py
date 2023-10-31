6a8c5493fef10998e1da6ebd18ccf3e6d0031063
trela <trela.art@gmail.com>
2023-10-31 22:07:40 +0100

    UPDATE : add method reset_position , and change logic in paddle bounce and bounce

--- pong_game/ball.py
+++ pong_game/ball.py
@@ -15,18 +15,32 @@ class Ball(Turtle):
         self.showturtle()
         self.penup()
         self.goto(0, 0)
+        self.step_y = 10
+        self.step_x = 10
+        self.speed = 0.1
 
     def ball_movement(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.step_x
+        new_y = self.ycor() + self.step_y
         self.goto(new_x, new_y)
         play.update()
 
-    def wall_detect(self):
-        """ Detect a wall collision and create a 90 angle rebound """
-        if int(self.ycor()) == -270 or self.ycor() == 270:
-            print("wall detect method")
+    def bounce(self, ):
+        """ Detect a wall collision and rebound """
+        self.step_y *= -1
+        new_y = self.ycor() + self.step_y
+        new_x = self.xcor() + self.step_x
+        self.goto(new_x, new_y)
 
-    def paddle_detect(self):
+    def paddle_bounce(self):
         """Detect ball position in range covered front edge of paddle if not in range call score increse"""
-        pass
+        self.step_x *= -1
+        new_y = self.ycor() + self.step_y
+        new_x = self.xcor() + self.step_x
+        self.goto(new_x, new_y)
+        self.speed = self.speed * 0.9
+
+    def reset_position(self):
+        self.paddle_bounce()
+        self.goto(0, 0)
+        self.speed = 0.1
