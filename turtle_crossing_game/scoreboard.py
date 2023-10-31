058a50cff6a2f92c0cb488b7bf3bff9bcda60a45
trela <trela.art@gmail.com>
2023-10-31 23:39:58 +0100

    UPDATE  : add points / level counter

--- turtle_crossing_game/scoreboard.py
+++ turtle_crossing_game/scoreboard.py
@@ -1,5 +1,21 @@
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 12, "normal")
+ALIGMENT = "center"
+from turtle import Turtle
 
+class Scoreboard(Turtle):
 
-class Scoreboard:
-    pass
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.score = 0
+        self.color("Black")
+        self.score_update()
+
+    def score_update(self):
+        self.write(f'Level :  {self.score}', False, align=ALIGMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.score_update()
