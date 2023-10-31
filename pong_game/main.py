39e567e77684f34a79b5c71daefabd0210d98ea3
trela <trela.art@gmail.com>
2023-10-31 21:04:22 +0100

    UPDATE :DELETE painting lines , remarks lines contains counters

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,3 +1,4 @@
+import time
 from turtle import Screen
 from paddle import Paddle
 from playground import Liner, Counter
@@ -11,37 +12,30 @@ screen.listen()
 screen.title("PONG GAME ")
 right_paddle = Paddle()
 left_paddle = Paddle()
-left_paddle.color("Blue")
 left_paddle.goto(-350,0)
-new_line = Liner()
-multiPlayer = False
-new_line.make_line()
-new_line.make_playfield()
-player_1_cnt = Counter()
-player_2_cnt = Counter()
-player_1_cnt.goto(50,260)
-player_2_cnt.goto(-50,260)
-player_2_cnt.clear()
-player_1_cnt.clear()
+
+multiPlayer = True
+# new_line.make_line()
+# new_line.make_playfield()
+# player_1_cnt = Counter()
+# player_2_cnt = Counter()
+# player_1_cnt.goto(50,260)
+# player_2_cnt.goto(-50,260)
+# player_2_cnt.clear()
+# player_1_cnt.clear()
 ball = Ball()
+# player_1_cnt.score_update()
+# player_2_cnt.score_update()
 
+screen.onkey(right_paddle.paddle_up, "Up")
+screen.onkey(right_paddle.paddle_down, "Down")
+screen.onkey(left_paddle.paddle_up, "w")
+screen.onkey(left_paddle.paddle_down, "s")
 is_game_On = True
-
-
 while is_game_On:
-    screen.onkey(right_paddle.paddle_up, "Up")
-    screen.onkey(right_paddle.paddle_down, "Down")
-    ball.ball_movement()
-    play.update()
-    player_1_cnt.score_update()
-    player_2_cnt.score_update()
-
-    if multiPlayer:
-        screen.onkey(left_paddle.paddle_up, "w")
-        screen.onkey(left_paddle.paddle_down, "s")
-
-
-
-    ball.wall_detect()
+    time.sleep(0.1)
     screen.update()
-    screen.exitonclick()
+    ball.ball_movement()
+
+
+screen.exitonclick()
