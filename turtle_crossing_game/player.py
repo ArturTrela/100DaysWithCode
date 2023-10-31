994938940043f4206e3ad0fe2e841386cbf1d6fb
trela <trela.art@gmail.com>
2023-10-31 23:41:05 +0100

    UPDATE  : add class player , move upward and check position for increase lvl after finish line

--- turtle_crossing_game/player.py
+++ turtle_crossing_game/player.py
@@ -1,7 +1,7 @@
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
-starting_position =(0, -280)
+
 from turtle import Turtle
 
 class Player(Turtle):
@@ -12,8 +12,15 @@ class Player(Turtle):
         self.color("black")
         self.penup()
         self.setheading(90)
-        self.goto(starting_position)
+        self.goto(STARTING_POSITION)
         self.showturtle()
+        self.onFinish =False
 
     def move(self):
-        self.forward(10)
+        self.forward(MOVE_DISTANCE)
+
+    def check_finish(self):
+
+        if self.ycor()>= FINISH_LINE_Y:
+            self.onFinish = True
+            self.goto(STARTING_POSITION)
