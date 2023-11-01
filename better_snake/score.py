7a50295db9600c81965e006b315788038d6b3eec
trela <trela.art@gmail.com>
2023-11-01 21:44:26 +0100

    UPDATE:add variable for contains high score value , remarks game ober method and add new one method reset required in this version code for work with files

--- better_snake/score.py
+++ better_snake/score.py
@@ -10,24 +10,31 @@ class Scoreboard(Turtle):
         super().__init__()
         self.penup()
         self.hideturtle()
+        self.high_score = 0
         self.score = 0
         self.goto(0, 250)
         self.color("White")
         self.score_update()
 
     def score_update(self):
-        self.write(f'Score = {self.score}', False, align=ALIGMENT, font=FONT)
+        self.write(f'Score = {self.score} High Score {self.high_score}', False, align=ALIGMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
         self.clear()
         self.score_update()
 
-    def game_over(self):
-        self.shape("square")
-        self.shapesize(200, 200)
-        self.color("grey")
-        self.showturtle()
-        self.goto(0, 0)
-        self.color("White")
-        self.write(f" GAME OVER.", False, align=ALIGMENT, font=FONT)
+    def reset(self):
+        if self.score >self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.clear()
+        self.score_update()
+    # def game_over(self):
+    #     self.shape("square")
+    #     self.shapesize(200, 200)
+    #     self.color("grey")
+    #     self.showturtle()
+    #     self.goto(0, 0)
+    #     self.color("White")
+    #     self.write(f" GAME OVER.", False, align=ALIGMENT, font=FONT)
