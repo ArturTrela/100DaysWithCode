fd6e06b829ed8d44cdde2d77d2ee61e0c77812fa
trela <trela.art@gmail.com>
2023-11-02 12:12:30 +0100

    UPDATE: add creating / writing to file last high score from game. And add method that initializes highscore var value from the file before start game

--- better_snake/score.py
+++ better_snake/score.py
@@ -17,7 +17,7 @@ class Scoreboard(Turtle):
         self.score_update()
 
     def score_update(self):
-        self.write(f'Score = {self.score} High Score {self.high_score}', False, align=ALIGMENT, font=FONT)
+        self.write(f'Score = {self.score} | Last High Score : {self.high_score}', False, align=ALIGMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
@@ -25,16 +25,27 @@ class Scoreboard(Turtle):
         self.score_update()
 
     def reset(self):
-        if self.score >self.high_score:
+        if self.score > int(self.high_score):
             self.high_score = self.score
+            score_to_file = self.high_score
+            high_score_file = open("high_score.txt", "w")
+            high_score_file.write(f'{score_to_file}')
+            high_score_file.close()
         self.score = 0
         self.clear()
         self.score_update()
-    # def game_over(self):
-    #     self.shape("square")
-    #     self.shapesize(200, 200)
-    #     self.color("grey")
-    #     self.showturtle()
-    #     self.goto(0, 0)
-    #     self.color("White")
-    #     self.write(f" GAME OVER.", False, align=ALIGMENT, font=FONT)
+
+    def last_high_score(self):
+        high_score_file = open("high_score.txt", "r")
+        lines = high_score_file.readlines()
+
+        # print(lines)
+        for line in lines:
+            print(line)
+        if len(lines) > 0:
+            score_from_file = lines[0]
+            self.high_score = score_from_file
+        else:
+            pass
+        self.score_update()
+        high_score_file.close()
