929f1cd0f646de51ad30362d84fd394cdd6b69f8
trela <trela.art@gmail.com>
2023-11-03 22:35:40 +0100

    UPDATE: Final version of code creating mail core. string manipulation, list method and relative adress training

--- mail_marge_project/Output/main.py
+++ mail_marge_project/Output/main.py
@@ -1,17 +1,18 @@
 """manualy created file and structure cause source file from repo Udemy is in iOS coding"""
 
-starting_letter = []
-invite_names = []
-
-with open("../Input/Letters/starting_letter.txt", "r") as letters:
-    for row in letters:
-        starting_letter.append(row)
-
-print(starting_letter)
+final_letter = []
+new_names = []
 
 with open("../Input/Names/invited_names.txt", "r") as invited:
-    for row in invited:
-        invite_names.append(row)
+    invite_names = invited.readlines()
+    for name in invite_names:
+        striped_name = name.strip()
+        new_names.append(striped_name)
+for name in new_names:
+    with open("../Input/Letters/starting_letter.txt", "r") as letters:
+        starting_letter = letters.read()
 
-# print(invite_names)
-# print(starting_letter)
+        final_letter = starting_letter.replace("[name]", name)
+        print(final_letter)
+        with open(f'./ReadyToSend/letter_for_{name}.txt', "w") as final:
+            final.writelines(final_letter)
