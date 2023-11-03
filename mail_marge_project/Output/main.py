13059dbb8027f130acb1f967e796f51efaa3a707
Artur <trela.art@gmail.com>
2023-11-03 16:51:49 +0100

    UPDATE: Add reading letters core and invited names from files located in another localization. Reading data has been stored in local list

--- mail_marge_project/Output/main.py
+++ mail_marge_project/Output/main.py
@@ -1,1 +1,17 @@
 """manualy created file and structure cause source file from repo Udemy is in iOS coding"""
+
+starting_letter = []
+invite_names = []
+
+with open("../Input/Letters/starting_letter.txt", "r") as letters:
+    for row in letters:
+        starting_letter.append(row)
+
+print(starting_letter)
+
+with open("../Input/Names/invited_names.txt", "r") as invited:
+    for row in invited:
+        invite_names.append(row)
+
+# print(invite_names)
+# print(starting_letter)
