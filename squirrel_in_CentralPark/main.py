9b40d85639250107fbde4ebfbedb64efbb0fcb70
Artur <trela.art@gmail.com>
2023-11-04 13:36:53 +0100

    UPDATE: code extract only squirrels with gray , cinnamon and black fur color and count qty. Collected data save as pandas Data frame created from dict

---
+++ squirrel_in_CentralPark/main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+fur_name = "Primary Fur Color"
+squirrel_ok = []
+data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20231104.csv")
+gray_squirrel_cnt = len(data[data[fur_name] == "Gray"])
+squirrel_ok.append(gray_squirrel_cnt)
+red_squirrel_cnt = len(data[data[fur_name] == "Cinnamon"])
+squirrel_ok.append(red_squirrel_cnt)
+black_squirrel_cnt = len(data[data[fur_name] == "Black"])
+squirrel_ok.append(black_squirrel_cnt)
+
+squirrel_dict = {
+    "Fur": ("Gray", "Red", "Black"),
+    "Counter": squirrel_ok
+}
+squirrel_df = pd.DataFrame(squirrel_dict)
+print(squirrel_df)
