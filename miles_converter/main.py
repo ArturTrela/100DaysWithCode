ada7e25989edebbee562d5f06b7d1684b0e37dd0
trela <trela.art@gmail.com>
2023-11-13 10:40:52 +0100

    UPDATE : final version code

--- miles_converter/main.py
+++ miles_converter/main.py
@@ -17,23 +17,16 @@ inputField.grid(column=0, row=3)
 
 def convert():
     ratio = 1.609344
-    user_input = int(inputField.get())
-    print(user_input)
-    result = user_input
+    user_input = inputField.get()
+    result = float(user_input) * ratio
+    print(result)
+    resultLabel = Label(text=f' Your result is equal : {result} kilometers ')
+    resultLabel.grid(column=0, row=5)
 
-    return result
 
 convertButton = Button(width=10, height=1, foreground="gray", background="White", font=("Arial", 10, "bold"),
-                       text="Convert", command= convert)
+                       text="Convert", command=convert)
 convertButton.grid(column=1, row=5)
-
-resultLabel =Label( text= " Your result is equal : ")
-resultLabel.grid(column=0, row=5)
-
-outputField =Text()
-outputField.insert(END, convert())
-outputField.grid(column = 0, row=10)
-outputField.config(width=15 , height= 1)
 
 
 
