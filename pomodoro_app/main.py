2d80994758cb3c16366524e9a1a539a003cbae7f
trela <trela.art@gmail.com>
2023-11-14 16:10:34 +0100

    INIT :change layout , add reset button

--- pomodoro_app/main.py
+++ pomodoro_app/main.py
@@ -6,7 +6,7 @@ PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
-FONT_NAME = ("Courier", 48 , )
+FONT_NAME = ("Courier", 24, )
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
@@ -32,17 +32,16 @@ class Window:
     def widgets(self):
         self.img = PhotoImage(file=self.image)
         label = tk.Label(self.root, image=self.img)
-        label.config(padx=0,pady=0)
-        label.grid(column=1, row=1)
+        label.grid(column=4, row=1)
 
         title_label = tk.Label(text=status[0], font=FONT_NAME)
         title_label.config(padx=0,pady=0)
-        title_label.grid(column=1, row=0)
+        title_label.grid(column=4, row=0)
 
-        start_btn = tk.Button(text = "START! ", background=PINK, foreground="Black", font=FONT_NAME)
-
-        start_btn.grid(column=1, row=2)
-
+        start_btn = tk.Button(text = "START", background=PINK, foreground="Black", font=FONT_NAME)
+        start_btn.grid(column=2, row=5)
+        reset_btn = tk.Button(text="RESET", background=PINK, foreground="Black", font=FONT_NAME)
+        reset_btn.grid(column=8, row=5)
 
 image = "tomato.png"
 Window(image)
