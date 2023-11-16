097c344548b7a4572a8cbe996507772d0b20a028
trela <trela.art@gmail.com>
2023-11-16 11:11:53 +0100

    UPDATE: init code to source and write code with UDEMY course

--- pomodoro_app/main.py
+++ pomodoro_app/main.py
@@ -11,38 +11,21 @@ FONT_NAME = ("Courier", 24,)
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-status = ["WORK", "BREAK"]
-worktime_preset = 25
-breaktime_preset = 5
-pomodoro_is_On = True
-remaining_time = datetime.time(0, 25, 0)
-
-window = tk.Tk()
-window.image ="tomato.png"
-window.title("POMODORO APP")
-window.minsize(240, 480)
+remaining_time = 25
+status =["WORK", "BREAK"]
 
 # # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-# act_time = datetime.time(0, minutes, seconds)
-def down():
-    minutes = 24
-    seconds = 59
-    while minutes >0:
-        for y in range(0,seconds):
-            time.sleep(0.1)
-            seconds -=1
-            remaining_time = (f'{minutes} : {seconds}')
-            print(remaining_time)
-        minutes -= 1
 
-    print(minutes)
-
-#
 # # ---------------------------- TIMER RESET ------------------------------- #
 
 # # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 # # ---------------------------- UI SETUP ------------------------------- #
+
+window = tk.Tk()
+window.image ="tomato.png"
+window.title("POMODORO APP")
+window.minsize(240, 480)
 #
 window.img = PhotoImage(file=window.image)
 label = tk.Label(window, image=window.img)
@@ -52,10 +35,10 @@ title_label = tk.Label(text=status[0], font=FONT_NAME)
 title_label.config(padx=0, pady=0)
 title_label.grid(column=4, row=0)
 
-start_btn = tk.Button(text="START", background=PINK, foreground="Black", font=FONT_NAME,command=down)
+start_btn = tk.Button(text="START", background=PINK, foreground="Black", font=FONT_NAME,command='')
 start_btn.grid(column=2, row=5)
 
-reset_btn = tk.Button(text="RESET", background=PINK, foreground="Black", font=FONT_NAME, )
+reset_btn = tk.Button(text="RESET", background=PINK, foreground="Black", font=FONT_NAME,command='' )
 reset_btn.grid(column=8, row=5)
 
 time_label = tk.Label(text = remaining_time, font=FONT_NAME, )
