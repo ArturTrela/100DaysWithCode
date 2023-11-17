119f03ac968ccf3a306c010bf6efd19dd0cf8229
trela <trela.art@gmail.com>
2023-11-17 13:11:25 +0100

    UPDATE : Add save and overlap data into file with password and web name

--- password_menager/main.py
+++ password_menager/main.py
@@ -4,9 +4,21 @@ from tkinter import *
 BACKGROUND = "#9EB8D9"
 FOREGROUND = "#7C93C3"
 FONTNAME = "Times New Roman "
+datalist=[]
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+
+    web = website_Input.get()
+    user = username_Input.get()
+    password = password_Field.get()
+    data = (f"WEB: {web}    |    USERNAME : {user}  |  PASSWORD: {password}\n ")
+    datalist.append(data)
+    with open("data.txt", "a") as file:
+        file.writelines(datalist)
+    # Clear field after insert data into file
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -30,17 +42,17 @@ password_Label.grid(column=0, row=3)
 
 website_Input = Entry(width=40, highlightbackground=FOREGROUND)
 website_Input.grid(column=1, row=1, columnspan=2, sticky="w")
-
-username_Input = Entry(width=40)
+website_Input.focus()
+username_Input = Entry(width=40, fg=FOREGROUND)
 username_Input.grid(column=1, row=2, columnspan=2, sticky="w")
-
+username_Input.insert(0,"testmail@elephant.com")
 password_Field = Entry(width=21, )
 password_Field.grid(column=1, row=3, sticky="w")
 
-generate_Button = Button(width=15, text="Generate Password", highlightcolor="blue", command='')
+generate_Button = Button(width=15,text="Generate Password", highlightcolor="blue", command='')
 generate_Button.grid(column=1, row=3, padx=131)
 
-add_Button = Button(width=34, text="ADD", padx=0, command='')
+add_Button = Button(width=34, text="ADD", padx=0, command=save)
 add_Button.grid(column=1, row=4, sticky="w")
 
 screen.mainloop()
