32cb75c867fc10a681880f25d039bed1cd4814dd
trela <trela.art@gmail.com>
2023-11-19 18:06:14 +0100

    INIT: TRY EXCEPT ELSE FINALL description

---
+++ try_except.py
@@ -0,0 +1,10 @@
+"""DESCRIPTION HOW THAT'S WORK"""
+
+try:
+    print("Place where an error posibble occures")
+except:
+    print("to do if an error occurs")
+else:
+    print("part if everything is OK")
+finally:
+    print("block use for e.g init variable after error or clear value of input fields")
