5fee8cb760f27164f8b1a6de23a149036878d552
trela <trela.art@gmail.com>
2023-11-21 22:09:09 +0100

    UPDATE: split code to section GUI / BACKGROUND LOGIC

--- FlashCard/main.py
+++ FlashCard/main.py
@@ -1,8 +1,13 @@
+# ___________IMPORTS_______________#
+
 from tkinter import *
 
+# __________CONSTANT_________#
 BACKGROUND_COLOR = "#B1DDC6"
 LANGUAGE_FONT = ("Arial", 40, "italic")
 WORD_FONT = ("Arial", 60, "bold")
+
+# __________GUI SETUP______________________#
 screen = Tk()
 screen.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 screen.title("Flash Card")
@@ -13,12 +18,12 @@ canvas.create_image(400, 264, image=imageCardFront)
 canvas.grid(column=1, row=1, columnspan=2)
 canvas.create_text(400, 150, text="French", font=LANGUAGE_FONT, fill="black")
 canvas.create_text(400, 263, text="WORD", font=WORD_FONT, fill="black")
-
 right_icon = PhotoImage(file="./images/right.png")
 canvas.create_image(600, 600, image=right_icon)
-
 wrong_icon = PhotoImage(file="./images/wrong.png")
 canvas.create_image(200, 600, image=wrong_icon)
+
+# ____________BACKGROUND LOGIC___________________#
 
 
 screen.mainloop()
