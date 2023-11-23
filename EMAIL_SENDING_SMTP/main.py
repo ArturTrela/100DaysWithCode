70348d914169368a6f507a34782913ee14af8cc0
RaspberryPi <arturtrela.automation@gmail.com>
2023-11-23 04:25:09 +0100

    Make a sender mail with random motivated quotes reading from file

--- EMAIL_SENDING_SMTP/main.py
+++ EMAIL_SENDING_SMTP/main.py
@@ -1,12 +1,30 @@
 import smtplib
-
+import datetime as dt
+import random
 from_google_pass = "apje xplv jdhg urkv"
 my_email = "arturtrela.automation@gmail.com"
 address_email = "trela.art@gmail.com"
-with smtplib.SMTP("smtp.gmail.com")as connection:
-    connection.starttls()
-    connection.login(user=my_email,password= from_google_pass)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs=address_email,
-        msg="Subject:hello !\n\n this is a body of my e-mail")
+quotes_list=[]
+now = dt.datetime.now()
+day = now.weekday()
+
+with open ("quotes.txt","r") as file:
+    quotes_list = file.readlines()
+
+quote_to_send = random.choice(quotes_list)
+
+print(quotes_list)
+def send_email():
+    with smtplib.SMTP("smtp.gmail.com")as connection:
+        connection.starttls()
+        connection.login(user=my_email,password= from_google_pass)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=address_email,
+            msg=f"Subject:Stay motivated !\n\n {quote_to_send}")
+
+
+print(day)
+if day == 3:
+    send_email()
+    print("E-mail was sended")
