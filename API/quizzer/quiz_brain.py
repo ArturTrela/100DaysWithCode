c38e38689fcaedc12eada6b3a7e10a8b724691ff
trela <trela.art@gmail.com>
2023-11-29 13:05:46 +0100

    UPDATE: Complete code for quizz with GUI.

--- API/quizzer/quiz_brain.py
+++ API/quizzer/quiz_brain.py
@@ -18,16 +18,14 @@ class QuizBrain:
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
         Q_TEXT = html.unescape(self.current_question.text)
-        user_answer = input(f"Q.{self.question_number}: {Q_TEXT} (True/False): ")
-        self.check_answer(user_answer)
+
+        return f"Q.{self.question_number}: {Q_TEXT} "
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
-        if user_answer.lower() == correct_answer.lower():
-            self.score += 1
-            print("You got it right!")
-        else:
-            print("That's wrong.")
 
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+        if user_answer == correct_answer:
+            self.score += 1
+            return True
+        else:
+            return False
--- API/quizzer/ui.py
+++ API/quizzer/ui.py
@@ -1,41 +1,64 @@
 import tkinter as tk
-from quiz_brain import Q_TEXT
+import tkinter.messagebox
 
+from quiz_brain import QuizBrain
 THEME_COLOR = "#375362"
 CANVA_BG = "#F8FFD2"
 
-question = Q_TEXT
-score = 0
+
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = tk.Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
-
-        self.score_label = tk.Label(text=f"Score : {score} ", font=("Arial", 20, "italic"), bg=THEME_COLOR, fg="White")
+        self.score = 0
+        self.score_label = tk.Label(text=f"Score : {self.score} ", font=("Arial", 20, "italic"), bg=THEME_COLOR, fg="White")
         self.score_label.config(padx=20, pady=20, )
-        self.score_label.grid(column=1, row=0,sticky=tk.E)
+        self.score_label.grid(column=1, row=0, sticky=tk.E)
 
         self.canvas = tk.Canvas()
 
-        self.canvas.config(width=300, height=250, highlightthickness=0,)
+        self.canvas.config(width=300, height=250, highlightthickness=0,bg=CANVA_BG )
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
-        self.question_text = self.canvas.create_text(150, 125, text="Temporary Text to Show", font=("Arial", 20, "italic"), fill="Black")
+        self.question_text = self.canvas.create_text(150, 125, width=280, text="Temporary",
+                                                     font=("Arial", 20, "italic"), fill="Black")
 
         true_btn = tk.PhotoImage(width=100, height=97, file="true.png")
-        self.true_Button = tk.Button(image=true_btn, command='',highlightthickness=0)
-        self.true_Button.grid(column=0, row=2,sticky=tk.NW)
+        self.true_Button = tk.Button(image=true_btn, command=self.right_button, highlightthickness=0)
+        self.true_Button.grid(column=0, row=2, sticky=tk.NW)
 
         wrong_btn = tk.PhotoImage(width=100, height=97, file="false.png")
-        self.wrong_Button = tk.Button(image=wrong_btn, command='',highlightthickness=0)
-        self.wrong_Button.grid(column=1, row=2,sticky=tk.NE)
+        self.wrong_Button = tk.Button(image=wrong_btn, command=self.wrong_button, highlightthickness=0)
+        self.wrong_Button.grid(column=1, row=2, sticky=tk.NE)
 
+        self.get_next_question()
         self.window.mainloop()
 
-    def true_answer(self):
-        pass
+    def get_next_question(self):
+        if not self.quiz.still_has_questions():
+            tkinter.messagebox.showinfo(title="QUIZ COMPLETE", message="You've completed the quiz\n"
+                                        f"Your final score is: {self.quiz.score}/{self.quiz.question_number}")
+        else :
+            self.score_label.config(text=f'Score : {self.quiz.score}')
+            self.canvas.config(bg=CANVA_BG)
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
 
-    def wrong_answer(self):
-        pass
+    def right_button(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def wrong_button(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+
+        else:
+            self.canvas.config(bg="red")
+
+        self.window.after(1000, self.get_next_question)
