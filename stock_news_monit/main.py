2513bf6e213c8d74230c1bb0636dc70d42560cb6
trela <trela.art@gmail.com>
2023-11-30 13:57:12 +0100

    UPDATE : Demo of project  -work but free version API's have limit up to 25 request . And NewsApi not allow using gmail/yahoo address email

--- stock_news_monit/main.py
+++ stock_news_monit/main.py
@@ -1,40 +1,50 @@
+import requests
+from twilio.rest import Client
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+"""Daily limit 25 request for API """
+stock_parameters = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": "7S8OZIMG7QUSMF6R",
+    "datatype": "json"
+}
+news_parameters = {
 
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
-
-#TODO 2. - Get the day before yesterday's closing stock price
-
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
-
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+}
+# stock_response = requests.get(STOCK_ENDPOINT, params=stock_parameters)
+# stock_response.raise_for_status()
+# stock_data = stock_response.json()
+# day_prev_close = float(stock_data['Time Series (Daily)']['2023-11-29']['4. close'])
+# two_day_close = float(stock_data['Time Series (Daily)']['2023-11-28']['4. close'])
 
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
-
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
+day_prev_close = 247.680
+two_day_close = 245.236
+absolut_diff = abs(day_prev_close - two_day_close)
+percent_diff = (two_day_close * absolut_diff) / 100
 
 
+def send_SMS():
+    account_sid = "ACe35debc71d618db5205a60f98130c727"
+    auth_token = "897ceea7e411db18dbd7e555e3e4c79b"
+    client = Client(account_sid, auth_token)
 
-#Optional TODO: Format the message like this: 
+    message = client.messages.create(
+        from_="+19562699689",
+        body=f"W dniu 2023-11-30 aukcje firmy {COMPANY_NAME} 🔻 5% {percent_diff} \n"
+             f"Jest to spowodowane prawdopodobnie faktem który znalazłem w sieci :  ",
+        to="+48519193352"
+    )
+    print(message.status)
+
+if percent_diff > 5:
+    print("Get News")
+    send_SMS()
+
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
