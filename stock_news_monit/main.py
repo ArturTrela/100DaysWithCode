82d50f8ac6a71d2d2ee308a7ddb10961aea48135
trela <trela.art@gmail.com>
2023-11-30 14:53:04 +0100

    UPDATE: add grab info from news api and add to text in SMS

--- stock_news_monit/main.py
+++ stock_news_monit/main.py
@@ -2,7 +2,7 @@ import requests
 from twilio.rest import Client
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-
+NEWS_API_KEY = "f6afe3854fe84560a79eff15d424dc62"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 """Daily limit 25 request for API """
@@ -12,9 +12,7 @@ stock_parameters = {
     "apikey": "7S8OZIMG7QUSMF6R",
     "datatype": "json"
 }
-news_parameters = {
 
-}
 # stock_response = requests.get(STOCK_ENDPOINT, params=stock_parameters)
 # stock_response.raise_for_status()
 # stock_data = stock_response.json()
@@ -28,6 +26,24 @@ absolut_diff = abs(day_prev_close - two_day_close)
 percent_diff = (absolut_diff / day_prev_close) * 100
 print(percent_diff)
 
+news_parameters ={
+    "apiKey": NEWS_API_KEY,
+    "q": "Tesla ",
+    "searchIn": "title",
+    "pageSize": 3,
+    "from": '2023-11-30',
+    "to": '2023-11-29',
+    "language": "en",
+    "sortBy": "popularity",
+    "totalResults": 3,
+}
+news_response = requests.get("https://newsapi.org/v2/everything", params=news_parameters)
+news_response.raise_for_status()
+news_data = news_response.json()
+
+news_title = (news_data["articles"][0]['title'])
+
+
 def send_SMS():
     account_sid = "ACe35debc71d618db5205a60f98130c727"
     auth_token = "897ceea7e411db18dbd7e555e3e4c79b"
@@ -35,8 +51,9 @@ def send_SMS():
 
     message = client.messages.create(
         from_="+19562699689",
-        body=f"W dniu 2023-11-30 aukcje firmy {COMPANY_NAME} 🔻 5% {percent_diff} \n"
-             f"Jest to spowodowane prawdopodobnie faktem który znalazłem w sieci :  ",
+        body=f"W dniu 2023-11-30 aukcje firmy {COMPANY_NAME} 🔻 5%   \n"
+             f"Jest to spowodowane prawdopodobnie faktem który znalazłem w sieci : \n"
+             f"{news_title} ",
         to="+48519193352"
     )
     print(message.status)
