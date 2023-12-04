e9c38e29a567d1b15c80476f6769f9495ed99f9c
trela <trela.art@gmail.com>
2023-12-04 13:59:42 +0100

    UPDATE: add object flight_search

--- cheap_flight_project/main.py
+++ cheap_flight_project/main.py
@@ -1,1 +1,6 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the
+# program requirements.
+
+from flight_search import FlightSearch
+
+flight_search = FlightSearch()
