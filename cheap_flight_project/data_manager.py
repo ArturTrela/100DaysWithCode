b000413ec395d107c68cf69bea4718a6a2079543
trela <trela.art@gmail.com>
2023-12-04 13:56:17 +0100

    INIT: take data from Google Sheet and save in variable as json

--- cheap_flight_project/data_manager.py
+++ cheap_flight_project/data_manager.py
@@ -1,3 +1,18 @@
+import requests
+import os
+
+BEARER = "Bearer @Automation@Automatyk2023"
+API_GET_END = "https://api.sheety.co/4e668c043186844af25349f9dcd69fdc/flightDeals/prices"
+sheet_headers = {
+    "Authorization": BEARER,
+}
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        pass
+
+    response = requests.get(url=API_GET_END, headers=sheet_headers)
+    response.raise_for_status()
+    data_from_sheets = response.json()
+    print(data_from_sheets)
