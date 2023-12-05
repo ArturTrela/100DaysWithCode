67d4139f72e49f2e783b62e9b2757524ddb4877d
trela <trela.art@gmail.com>
2023-12-05 11:59:18 +0100

    UPDATE: add object sheet_data (Google Sheets API) and print values form other modules for check correct work

--- cheap_flight_project/main.py
+++ cheap_flight_project/main.py
@@ -1,6 +1,11 @@
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the
 # program requirements.
-
 from flight_search import FlightSearch
-
+from data_manager import DataManager
 flight_search = FlightSearch()
+founded_flight = flight_search.flightCheck()
+print(founded_flight)
+
+sheet_data = DataManager()
+datasheet = sheet_data.take_data_from_sheet()
+print(datasheet)
