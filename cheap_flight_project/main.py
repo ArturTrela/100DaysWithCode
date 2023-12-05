0ec5c12b77a4aa7bfdbc8734036c18884ccd42f1
trela <trela.art@gmail.com>
2023-12-05 14:15:34 +0100

    UPDATE: add logic to update code in file if required.

--- cheap_flight_project/main.py
+++ cheap_flight_project/main.py
@@ -2,10 +2,33 @@
 # program requirements.
 from flight_search import FlightSearch
 from data_manager import DataManager
-flight_search = FlightSearch()
-founded_flight = flight_search.flightCheck()
-print(founded_flight)
 
-sheet_data = DataManager()
-datasheet = sheet_data.take_data_from_sheet()
-print(datasheet)
+CITIES = ['Paris', 'Berlin', 'Tokyo', 'Sydney', 'Istanbul', 'Kuala Lumpur', 'New York', 'San Francisco', 'Cape Town']
+CODES =[]
+flightSearch = FlightSearch()
+# founded_flight = flight_search.flightCheck()
+# print(founded_flight)
+
+# _________________ TAKE DATA FROM GOOGLE SHEETS_____________
+sheetData = DataManager()
+datasheet = sheetData.take_data_from_sheet()
+cities_name =[]
+for city in datasheet:
+    city_name = city['city']
+    ident = city['id']
+    miasta ={
+        city_name : ident
+    }
+    cities_name.append(miasta)
+
+# ____________UPDATE AIRPORT'S CODES IN FILE _____________
+is_required_update_codes = False
+if is_required_update_codes:
+    ident = 2
+    for name in CITIES:
+        code = flightSearch.takeName(name)
+        CODES.append(code)
+        sheetData.codeFilling( code, name, ident)
+        ident +=1
+else:
+    print(F'File is actual ')
