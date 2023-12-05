f9211cbb89782d6c3898c47592eef34195f90d34
trela <trela.art@gmail.com>
2023-12-05 14:14:51 +0100

    UPDATE: add method code filling in sheets request PUT

--- cheap_flight_project/data_manager.py
+++ cheap_flight_project/data_manager.py
@@ -4,14 +4,37 @@ import requests
 class DataManager:
     def __init__(self):
         self.BEARER = "Bearer @Automation@Automatyk2023"
-        self.API_GET_END = "https://api.sheety.co/4e668c043186844af25349f9dcd69fdc/flightDeals/prices"
+        self.API_END = "https://api.sheety.co/4e668c043186844af25349f9dcd69fdc/flightDeals/prices"
         self.sheet_headers = {
             "Authorization": self.BEARER,
         }
 
     def take_data_from_sheet(self):
-        response = requests.get(url=self.API_GET_END, headers=self.sheet_headers)
+        response = requests.get(url=self.API_END, headers=self.sheet_headers)
         response.raise_for_status()
         data_from_sheets = response.json()
 
-        return data_from_sheets
+        return data_from_sheets['prices']
+
+    def codeFilling(self,code,name,ident):
+        """
+        {'prices': [{'city': 'Paris', 'iataCode': '', 'lowestPrice': 54, 'id': 2},
+
+         parameters = {
+            'prices':{
+                'city':name,
+                'iataCode':code,
+            }
+        """
+
+        parameters = {
+            'price':{
+                'city':name,
+                'iataCode':code,
+            }
+
+        }
+        r = requests.put(url=f'{self.API_END}/{ident}', json=parameters,headers=self.sheet_headers)
+        r.raise_for_status()
+        insert_result = r.json()
+        return insert_result
