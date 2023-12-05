22fb49c18983f54ad827cf78b651ac59352b9e6d
trela <trela.art@gmail.com>
2023-12-05 11:58:10 +0100

    UPDATE: close all code in method and delete not using code

--- cheap_flight_project/flight_search.py
+++ cheap_flight_project/flight_search.py
@@ -1,39 +1,46 @@
 import requests
 import datetime
+
 API_KEY = "DoF_upgTb1msmIo7PxLjoMmdTef5GWjI"
 API_ID = ""
 API_SEARCH_END = "https://api.tequila.kiwi.com/v2/search"
 API_LOCATION_END = "https://api.tequila.kiwi.com/locations/id"
 today = datetime.datetime.today()
 today = today.strftime("%d/%m/%Y")
-print(today)
-header = {
-    'apikey':API_KEY,
-}
-params = {
-    "id" : "KTW",
-    "active_only": "true",
-    "limit": 20,
 
-}
-search_params = {
-    "fly_from" : "KTW",
-    "date_from" : today,
-    "date_to" :"07/12/2023"
-}
-# response = requests.get(url=API_LOCATION_END, params=params, headers=header)
-# response.raise_for_status()
-# resp = response.json()
-# print(resp)
-
-search_response = requests.get(url=API_SEARCH_END, params=search_params ,headers=header)
-search_response.raise_for_status()
-search_data = search_response.json()
-cityCode_to = search_data['data'][0]['cityCodeTo']
-cityName_to = search_data['data'][0]['cityTo']
-flight_price = search_data['data'][0]['price']
-
-print(cityName_to , cityCode_to , flight_price)
 
 class FlightSearch:
-    pass
+
+    def __init__(self):
+        self.header = {
+            "apikey": API_KEY,
+        }
+        self.flight_values =[]
+
+    def flightCheck(self):
+        search_params = {
+            "fly_from": "KTW",
+            "date_from": today,
+            "date_to": "07/12/2023",
+            # "curr" : tutaj wpisaæ f-string z kodami IATA z googla,
+            "limit": "10",
+        }
+        search_response = requests.get(url=API_SEARCH_END, params=search_params, headers=self.header)
+        search_response.raise_for_status()
+        search_data = search_response.json()
+
+        #### Concept of grab value ####
+        for data in search_data['data']:
+            cityCode_to = data['cityCodeTo']
+            cityName_to = data['cityTo']
+            flight_price = data['price']
+            flight_price_currency = data['conversion']
+            flight_params = {
+                "city": cityName_to,
+                "cityCode": cityCode_to,
+                "price": flight_price,
+                "currency": flight_price_currency,
+            }
+            self.flight_values.append(flight_params)
+
+        return self.flight_values
