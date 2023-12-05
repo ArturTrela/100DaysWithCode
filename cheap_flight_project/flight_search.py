cbfda0d4bb4ca2b3ee6a2ab1b8411db4fb32c863
trela <trela.art@gmail.com>
2023-12-05 14:14:06 +0100

    UPDATE: add method to take a code airport's from api with using name of City

--- cheap_flight_project/flight_search.py
+++ cheap_flight_project/flight_search.py
@@ -4,7 +4,7 @@ import datetime
 API_KEY = "DoF_upgTb1msmIo7PxLjoMmdTef5GWjI"
 API_ID = ""
 API_SEARCH_END = "https://api.tequila.kiwi.com/v2/search"
-API_LOCATION_END = "https://api.tequila.kiwi.com/locations/id"
+API_LOCATION_END = "https://api.tequila.kiwi.com/locations/query"
 today = datetime.datetime.today()
 today = today.strftime("%d/%m/%Y")
 
@@ -22,7 +22,7 @@ class FlightSearch:
             "fly_from": "KTW",
             "date_from": today,
             "date_to": "07/12/2023",
-            # "curr" : tutaj wpisaæ f-string z kodami IATA z googla,
+            # "curr" : tutaj wpisac f-string z kodami IATA z googla,
             "limit": "10",
         }
         search_response = requests.get(url=API_SEARCH_END, params=search_params, headers=self.header)
@@ -44,3 +44,15 @@ class FlightSearch:
             self.flight_values.append(flight_params)
 
         return self.flight_values
+
+    def takeName(self,name):
+        name_params ={
+            "term" : name
+        }
+
+        response_iata= requests.get(url=API_LOCATION_END, params=name_params, headers=self.header)
+        response_iata.raise_for_status()
+        result = response_iata.json()
+        city_code = result['locations'][0]['code']
+
+        return city_code
