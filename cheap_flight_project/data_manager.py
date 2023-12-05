dce5f731d164efe1927a9b2d64df4c3ed00c3676
trela <trela.art@gmail.com>
2023-12-05 11:57:24 +0100

    UPDATE: close all code in method

--- cheap_flight_project/data_manager.py
+++ cheap_flight_project/data_manager.py
@@ -1,18 +1,17 @@
 import requests
-import os
-
-BEARER = "Bearer @Automation@Automatyk2023"
-API_GET_END = "https://api.sheety.co/4e668c043186844af25349f9dcd69fdc/flightDeals/prices"
-sheet_headers = {
-    "Authorization": BEARER,
-}
 
 
 class DataManager:
     def __init__(self):
-        pass
+        self.BEARER = "Bearer @Automation@Automatyk2023"
+        self.API_GET_END = "https://api.sheety.co/4e668c043186844af25349f9dcd69fdc/flightDeals/prices"
+        self.sheet_headers = {
+            "Authorization": self.BEARER,
+        }
 
-    response = requests.get(url=API_GET_END, headers=sheet_headers)
-    response.raise_for_status()
-    data_from_sheets = response.json()
-    print(data_from_sheets)
+    def take_data_from_sheet(self):
+        response = requests.get(url=self.API_GET_END, headers=self.sheet_headers)
+        response.raise_for_status()
+        data_from_sheets = response.json()
+
+        return data_from_sheets
