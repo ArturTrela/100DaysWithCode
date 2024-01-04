0004ab87bcaf0ee24b23a7e6fb2636cee52f365c
trela <trela.art@gmail.com>
2024-01-04 21:27:36 +0100

    INIT: add few tips and tricks to init file

--- WEB_SCRAPING/main.py
+++ WEB_SCRAPING/main.py
@@ -5,4 +5,16 @@ with open("website.html") as file:
 
 
 soup = BeautifulSoup(contents,'html.parser')
-print(soup.prettify())
+all_anchor_tags=soup.find_all(name="a")
+# print(all_anchor_tags)
+# """Search from tags only text or text with links with using method 'get' """
+# for tag in all_anchor_tags:
+#     # print(tag.getText())
+#     print(f'{tag.getText()} {tag.get("href")}')
+#
+# heading =soup.find(name= "h1" , id="name")
+# print(heading)
+
+# How to skip "class form html file " --> add underscroll after word "class"
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading)
