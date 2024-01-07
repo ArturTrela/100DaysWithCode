bb72f6b7f3100840bfb2593cc53488a47a22c988
trela <trela.art@gmail.com>
2024-01-07 21:39:38 +0100

    INIT: exercise BeautyfulSoup , scrapping I'm need to try more times

--- WEB_SCRAPING/main.py
+++ WEB_SCRAPING/main.py
@@ -1,20 +1,21 @@
+import requests
 from bs4 import BeautifulSoup
-import lxml
-with open("website.html") as file:
-    contents = file.read()
 
 
-soup = BeautifulSoup(contents,'html.parser')
-all_anchor_tags=soup.find_all(name="a")
-# print(all_anchor_tags)
-# """Search from tags only text or text with links with using method 'get' """
-# for tag in all_anchor_tags:
-#     # print(tag.getText())
-#     print(f'{tag.getText()} {tag.get("href")}')
-#
-# heading =soup.find(name= "h1" , id="name")
-# print(heading)
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-# How to skip "class form html file " --> add underscroll after word "class"
-section_heading = soup.find(name="h3", class_="heading")
-print(section_heading)
+# Write your code below this line 👇
+
+response = requests.get(URL)
+web_data = response.text
+
+titles =[]
+soup = BeautifulSoup(web_data,"html.parser")
+
+
+title = soup.find(name="h3", class_="title").text.encode("utf-8")
+for title in soup:
+    tytuł = soup.getText("title")
+    titles.append(tytuł)
+
+print(titles)
