474d20a815644930e438d1d004aa606ae80bf4c8
trela <trela.art@gmail.com>
2024-01-08 12:05:31 +0100

    INIT: update code correctly scrap data from website

--- TimeMachine/main.py
+++ TimeMachine/main.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 
 START_URL ="https://www.billboard.com/charts/hot-100"
 
-# user_prompt = input("Which year do you want to travel to ? Date format is : YYYY-MM-DD ")
-user_prompt = "2020-06-01"
+user_prompt = input("Which year do you want to travel to ? Date format is : YYYY-MM-DD ")
+# user_prompt = "2020-06-01"
 URL = f'{START_URL}/{user_prompt}'
 print(URL)
 
@@ -12,6 +12,16 @@ response =requests.get(URL)
 web_resp = response.text
 
 soup = BeautifulSoup(web_resp,features="html.parser")
-title = soup.find_all(name = "h3", )
-print(title)
+song_name_spans = soup.select("div ul li ul h3")
+artist_name_spans = soup.select("div ul li ul span")
+song_names = [song.getText().strip() for song in song_name_spans]
+songs_file =[]
+for x in range(1,100):
+    song_new = f"{x}) {song_names[x]} \n "
+    songs_file.append(song_new)
+print(song_names)
+
+with open(f"file_song_{user_prompt}.txt", "w") as file:
+    file.writelines(songs_file)
+
 
