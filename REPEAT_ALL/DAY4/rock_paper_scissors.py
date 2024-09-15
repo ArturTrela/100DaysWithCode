6927fdf706eaab52ad7717d37e3180e1f9960da1
Artur Trela <trela.art@gmail.com>
2024-09-16 00:28:08 +0200

    REPEAT DAY4 - add type for user_guess

--- REPEAT_ALL/DAY4/rock_paper_scissors.py
+++ REPEAT_ALL/DAY4/rock_paper_scissors.py
@@ -2,4 +2,4 @@ import random
 rock_asci = """"""
 paper_asci = """"""
 scissors_asci = """"""
-user_guess = input('What you choose : 0 for rock , 1 for paper , 2 for scissors ')
+user_guess = int(input('What you choose : 0 for rock , 1 for paper , 2 for scissors '))
