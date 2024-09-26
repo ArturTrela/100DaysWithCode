1f87cd8c5c18bf4a1887aa5f39f53db45e5e38d8
Artur Trela <trela.art@gmail.com>
2024-09-26 02:59:01 +0200

    REPEAT DAY16 - modify attributes object and change methods in object

--- REPEAT_ALL/DAY16/main.py
+++ REPEAT_ALL/DAY16/main.py
@@ -1,4 +1,14 @@
 """OOP remember knowledge  """
 from another_module import another_variable
+from turtle import Turtle ,Screen
 
 print(another_variable)
+
+john = Turtle()
+john.shape("turtle")
+john.color("Blue")
+john.forward(100)
+
+screen =Screen()
+print(screen.canvheight)
+screen.exitonclick()
