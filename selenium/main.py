f5d3ef43be7ad96c6866ecef0641be544624e2c6
ArturTrela <trela.art@gmail.com>
2024-10-14 08:49:56 +0200

    create driver for chrome web browser and check introduction knowlage from course

--- selenium/main.py
+++ selenium/main.py
@@ -1,1 +1,15 @@
 """ File to show how does work library Selenium WebDriver for automation Web browser operation"""
+
+from selenium import webdriver
+
+# keep chrome open
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True )
+driver = webdriver.Chrome(options=chrome_options)
+driver.get('https://www.amazon.com')
+
+# close single tab
+# driver.close()
+# close full browser
+# driver.quit()
