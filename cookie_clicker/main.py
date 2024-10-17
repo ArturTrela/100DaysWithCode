9630ba81a39a6c6ce96ec8b1d958fd0898d50237
ArturTrela <trela.art@gmail.com>
2024-10-17 20:42:23 +0200

    add click cookies

--- cookie_clicker/main.py
+++ cookie_clicker/main.py
@@ -1,7 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import time
 opti = webdriver.ChromeOptions()
 opti.add_experimental_option("detach", True)
 drive = webdriver.Chrome(options=opti)
 drive.get('https://orteil.dashnet.org/experiments/cookie/')
+
+while True:
+    cookie = drive.find_element(By.ID, "cookie")
+    money = (drive.find_element(By.ID, 'money').text)
+    buyGrandma = drive.find_element(By.ID, "buyGrandma")
+    cookie.click()
+    if money >= "100":
+        buyGrandma.click()
+
+
+drive.close()
