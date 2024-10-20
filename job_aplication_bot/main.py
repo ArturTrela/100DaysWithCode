bd492cd966f256c43f549bdba1311ee8c966a366
ArturTrela <trela.art@gmail.com>
2024-10-21 00:51:56 +0200

    FIX: dodanie zakonczenia aplikacji

--- job_aplication_bot/main.py
+++ job_aplication_bot/main.py
@@ -1,6 +1,4 @@
 """ Bot for Job aplication from Udemy course - day 49 """
-from fileinput import close
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -10,6 +8,16 @@ import time
 opt = webdriver.ChromeOptions()
 opt.add_experimental_option('detach', True)
 drive = webdriver.Chrome(opt)
+
+def abort_application():
+    # Click Close Button
+    close_button = drive.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
+    close_button.click()
+
+    time.sleep(2)
+    # Click Discard Button
+    discard_button = drive.find_elements(by=By.CLASS_NAME, value="artdeco-modal__confirm-dialog-btn")[1]
+    discard_button.click()
 
 def open_site():
     drive.get('https://www.linkedin.com/jobs/search/?currentJobId=4051717426&f_AL=true&f_E=1%2C2&keywords=Python%20Developer&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true')
@@ -56,10 +64,44 @@ def zapisz_oferte():
     zapisz_btn = drive.find_element(By.CSS_SELECTOR,'jobs-save-button artdeco-button artdeco-button--secondary artdeco-button--3')
     zapisz_btn.click()
 
+def aplikuj_wszystkie():
+    try :
+        list_oferty = drive.find_elements(By.CLASS_NAME,'job-card-container--clickable')
+        print(list_oferty)
+        for oferta in list_oferty:
+            oferta.click()
+            apply_btn = drive.find_element(By.CSS_SELECTOR,".jobs-s-apply button")
+            apply_btn.click()
+            time.sleep(2)
+            phone = drive.find_element(By.CSS_SELECTOR,' input[id*=phoneNumber]')
+            if phone.text =='':
+                phone.send_keys('519193352')
+            next = drive.find_element(By.CSS_SELECTOR,'footer button')
+            next.click()
+            submit_button = drive.find_element(by=By.CSS_SELECTOR, value="footer button")
+            if submit_button.get_attribute("data-control-name") == "continue_unify":
+                abort_application()
+                print("Complex application, skipped.")
+                continue
+            else:
+                # Click Submit Button
+                print("Submitting job application")
+                submit_button.click()
+            time.sleep(2)
+            # Click Close Button
+            close_button = drive.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
+            close_button.click()
+    except NoSuchFrameException :
+        abort_application()
+        print("No application button, skipped.")
 
+
+    finally:
+        print('Zapisano wszystkie oferty')
 open_site()
 close_popup()
 odrzuc_cookies()
 logowanie()
+aplikuj_wszystkie()
 time.sleep(100)
 drive.close()
