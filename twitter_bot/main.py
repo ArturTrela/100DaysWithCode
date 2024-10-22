5ec6e374a730c11d72b704a6f91b5b2fc1dec1f7
ArturTrela <trela.art@gmail.com>
2024-10-23 01:24:19 +0200

    FIX : PEP8 refactor code

--- twitter_bot/main.py
+++ twitter_bot/main.py
@@ -53,5 +53,4 @@ class InternetSpeedTwitterBot():
         pass_input.send_keys(Keys.ENTER)
 bot = InternetSpeedTwitterBot()
 wynik_testu = bot.get_internet_speed()
-wynik_testu = "test"
 bot.tweet_at_provider(wynik_testu)
