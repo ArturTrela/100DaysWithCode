b516e31f6a02063f661a523d1f3ff3da96f06a4c
ArturTrela <trela.art@gmail.com>
2024-11-26 22:18:16 +0100

    dodanie dekoratorów do funkcji "bye"

--- Flask_Framework/hello.py
+++ Flask_Framework/hello.py
@@ -7,7 +7,25 @@ app.debug = True
 def hello_world():
     return 'hello_world'
 
+def make_bold(return_z_funkcji_nadrzednej):
+    def wrapper():
+        return "<b>" + return_z_funkcji_nadrzednej() + "</b>"
+    return wrapper
+
+def make_underlined(return_z_funkcji_nadrzednej):
+    def wrapper():
+        return "<u>" + return_z_funkcji_nadrzednej() + "</u>"
+    return wrapper
+
+def make_emphasis(return_z_funkcji_nadrzednej):
+    def wrapper():
+        return "<em>" + return_z_funkcji_nadrzednej() + "</em>"
+    return wrapper
+
 @app.route('/bye')
+@make_bold
+@make_underlined
+@make_emphasis
 def bye():
     return 'Bye-Bye - działa'
 
