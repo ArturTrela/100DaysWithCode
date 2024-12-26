972c172fa47aa11f2405d28c4ba955db4bcaabfb
ArturTrela <trela.art@gmail.com>
2024-12-26 23:19:34 +0100

    final code from 57 day - complete . One problem during coding -> iteration via list where list element was a dictkey - check issue with GPT

--- day-57-starting-files-blog-templating/main.py
+++ day-57-starting-files-blog-templating/main.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
+@app.route('/post/<id>')
+def show_post(id):
+    blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
+    posts = requests.get(blog_url).json()
+    new = [_ for _ in posts if _['id'] == int(id)]
+    print(type(new))
+    return render_template('post.html',new = new)
+
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
+    posts = requests.get(blog_url).json()
+    return render_template("index.html", posts=posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
