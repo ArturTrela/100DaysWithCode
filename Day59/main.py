005abc2b632c93c5d5a5b2bbceb04888ba943f65
ArturTrela <trela.art@gmail.com>
2024-12-29 15:27:51 +0100

    complete code with get data from api dynamic rendered into index.html and make a separately post site

--- Day59/main.py
+++ Day59/main.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 app.debug =True
+api_endpoint = 'https://api.npoint.io/4c5f52246a8e640796fe'
+
+@app.route('/post/<id>')
+def post_view(id):
+    response = requests.get(api_endpoint)
+    posty = response.json()
+    post_data =[post for post in posty if post['id'] == int(id)]
+    print(f'post data to {post_data}')
+    return render_template('post.html', post_data=post_data)
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html")
+
+    return render_template("contact.html" )
 
 @app.route('/about')
 def about():
@@ -14,7 +24,10 @@ def about():
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    response = requests.get(api_endpoint)
+    posty = response.json()
+    print(posty)
+    return render_template("index.html", posty=posty)
 
 if __name__ == "__main__":
     app.run()
