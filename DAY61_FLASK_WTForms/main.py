9b4aff377e1988f4ea529073ec30f8f7e5db37e9
ArturTrela <trela.art@gmail.com>
2025-01-01 14:09:01 +0100

    complete create form with using Bootstap-Flask

--- DAY61_FLASK_WTForms/main.py
+++ DAY61_FLASK_WTForms/main.py
@@ -1,25 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import  PasswordField, SubmitField, EmailField, validators
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap5
 
 
 app = Flask(__name__)
-
 app.secret_key = "nalesnik"
+bootstrap = Bootstrap5(app)
 
-class MyForm(FlaskForm):
-    email = StringField('email')
-    password = StringField('password')
+class LoginForm(FlaskForm):
+    email = EmailField(label='email', validators=[DataRequired(),Length(min=6, max=120),Email()])
+    password = PasswordField(label='password', validators=[DataRequired(),Length(min=8)])
+    submit = SubmitField(label = 'Log in')
 
-@app.route('/success')
-def isLogin():
-    return "Zalogowano"
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form =MyForm()
-
-    return render_template('login.html', form = form )
+    login_form =LoginForm()
+    if request.method == "POST":
+        if login_form.validate_on_submit():
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template('login.html',form =login_form )
 
 
 @app.route("/")
